847da4eacf24d7446cbbc7274ea810db3387dcae
digitest <gigabayt@gmail.com>
2015-10-25 19:29:43 +0300

    lazer_subscriber üzerinde dct level 4 alınacak şekilde denendi

--- laser_subscriber.m
+++ laser_subscriber.m
@@ -25,15 +25,36 @@ function [ output_args ] = laser_subscriber(src, msg)
 %     coefs = cwt(sinyal,scales,wname);
 %     wscalogram('image',coefs,'scales',scales,'ydata',sinyal);
     
-    figure(2)
-    coefs2 = cwt(sinyal,scales2,wname);
-    wscalogram('image',coefs2,'scales',scales2,'ydata',sinyal);
+%     figure(2)
+%     coefs2 = cwt(sinyal,scales2,wname);
+%     wscalogram('image',coefs2,'scales',scales2,'ydata',sinyal);
     
+     
     
-    figure(3)
+%             [ca1,cd1] = dwt(sinyal,'db2');
+%             [ca2,cd2] = dwt(ca1,'db2');
+%             [ca3,cd3] = dwt(ca2,'db2');
+%             [ca4,cd4] = dwt(ca3,'db2');
+%             son=resample(cd4,length(sinyal),length(cd4));
+%             plot(son);
+       
+sinyal=double(sinyal);
+      figure(3)
+subplot(2,1,1)       
+plot(sinyal);
+    [ca1,cd1] = dwt(sinyal,'db2');
+    [ca2,cd2] = dwt(ca1,'db2');
+    [ca3,cd3] = dwt(ca2,'db2');
+    [ca4,cd4] = dwt(ca3,'db2');
+son=resample(cd4,length(sinyal),length(cd4));
+subplot(2,1,2)
+plot(son);
+
 %     coefs3 = cwt(sinyal,scales3,wname);
 %     wscalogram('image',coefs3,'scales',scales3,'ydata',sinyal);
-    plot(coefs2)
+    %plot(coefs2)
+    
+    
 
 end
 
